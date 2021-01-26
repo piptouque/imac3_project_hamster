dad4979ca8719a0917972a5e593fe676ab92ba50
piptouque <63.pthiel@gmail.com>
2021-01-26 19:38:42 +0100

    [Fix]: Fixed display of lightbulb.

--- ampoule/mockup.js
+++ ampoule/mockup.js
@@ -2,83 +2,67 @@
 var img;
 var video;
 var mask;
-var pg;
 
 var prevFrame;
-var threshold = 25;
-var step = 1; // seconds.
-
-var lightbulbWidth = 100;
-var lightbulbHeight = 160;
-var lightbulbHeightOffset = 40;
-
+var threshold = 120;
 var c = 0;
-var acc = 0;
-var move = false;
+
+var w = window.innerWidth  * 0.8;
+var h = window.innerHeight;
+
+var lightbulbWidth  = w * 0.1;
+var lightbulbHeight =  lightbulbWidth * 1.6;
+var lightbulbHeightOffset = 0.28;
 
 function preload(){
-  img  = loadImage('assets/lamp.png');
+  img = loadImage('assets/lamp.png');
   mask = loadImage('assets/lamp_mask2.png');
-
 }
 
 function setup() {
-  createCanvas(1920, 1080);
+  createCanvas(w, h);
+
   pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(width, height);
+  video.size(w, h);
   video.hide();
   prevFrame = createImage(video.width, video.height/*,RGB*/);
 	
+  //let c = color(255,255,0);
+  //fill(c);
   noStroke();
-
+  
 }
-
 function draw() {
   background(255,255,255);
   translate(video.width, 0);
   scale(-1,1);
   noTint();
-
-  pg = createGraphics(width, height);
-
   
   image(video,0, 0, video.width, video.height);
 
-  pg.fill(color(255,255,0));
+  pg = createGraphics(width, height);
+  pg.fill(color(255, 255, 0));
 
-  pg.rect(0, height * (1 - c / 700), width, height);
+  pg.rect(
+    0,
+    height * (1 - c / 700) - height * lightbulbHeightOffset,
+    width,
+    height
+  );
   var p = pg.get();
 
   p.mask(mask);
-
-  image(img, 0, 0, lightbulbWidth, lightbulbHeight);
-
-  image(p, 
-    0,
-    0, 
-    lightbulbWidth, 
-    lightbulbHeight
-  );
-
-  var deltaTime = 1 / 24; // fixme
   
-  acc += deltaTime;
+  image(img,0,0, lightbulbWidth, lightbulbHeight);
+  image(p, 0, 0, lightbulbWidth, lightbulbHeight);
   
-  if (acc >= step) {
-    acc = 0;
-    computeLight();
-  }
-
-}
-
-function computeLight() {
-
+ 
   video.loadPixels();
   prevFrame.loadPixels();
 
-  var amountMoved = 0.0;
-
+  var move  = false;
+  
   for (var y = 0; y < height; y++) {
     for (var x = 0; x < width; x++) {
       // Step 1, what is the location into the array
@@ -95,23 +79,22 @@ function computeLight() {
       var b2 = video.pixels[loc + 2];
     
       var diff = dist(r1, g1, b1, r2, g2, b2);
-
-      amountMoved += diff;
+      
+      if (diff > threshold)
+      {
+        move = true;
+      }
     }
   }
 
-  amountMoved /= width * height;
-  console.log(amountMoved);
-
-  move = amountMoved > threshold;
 
   if (move && c<700){
     c = c+10;
   }
-  else if (c>0) {
+  else if (c>0) {
     c = c-10;
   }
-  
-  prevFrame.copy(video, 0, 0, video.width, video.height, 0, 0, video.width, video.height); // Before we read the new frame, we always save the previous frame for comparison!
-}
 
+  prevFrame.copy(video, 0, 0, video.width, video.height, 0, 0, video.width, video.height); // Before we read the new frame, we always save the previous frame for comparison!
+
+}
